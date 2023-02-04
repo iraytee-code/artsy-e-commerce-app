e96dad9f1889ab74f993d9ce13ccaa3012e57848
iraytee-code <adeniranhope21@gmail.com>
2023-02-04 19:04:40 +0100

    add framer implementation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,18 @@ import React, { Fragment } from "react";
 import NavBar from "../components/NavBar";
 import Banner from "../layouts/Banner";
 import HeroCarousel from "../layouts/HeroCarousel";
+import Transitions from "../utils/Transitions";
 
 const Home = () => {
   return (
     <Fragment>
-      <NavBar />
-      <Banner />
-      <div className="md:overflow-x-scroll">
-        <HeroCarousel />
-      </div>
+      <Transitions>
+        <NavBar />
+        <Banner />
+        <div className="md:overflow-x-scroll">
+          <HeroCarousel />
+        </div>
+      </Transitions>
     </Fragment>
   );
 };
