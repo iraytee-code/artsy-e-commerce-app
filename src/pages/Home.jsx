1be388ea586adada0544a2e5270341d1b7ee432b
iraytee-code <adeniranhope21@gmail.com>
2023-02-05 22:46:11 +0100

    remove transition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,15 @@ import React, { Fragment } from "react";
 import NavBar from "../components/NavBar";
 import Banner from "../layouts/Banner";
 import HeroCarousel from "../layouts/HeroCarousel";
-import Transitions from "../utils/Transitions";
 
 const Home = () => {
   return (
     <Fragment>
-      <Transitions>
         <NavBar />
         <Banner />
         <div className="md:overflow-x-scroll">
           <HeroCarousel />
         </div>
-      </Transitions>
     </Fragment>
   );
 };
