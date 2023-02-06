003302b3de3767f74353003dcfbfae79dd710411
iraytee-code <adeniranhope21@gmail.com>
2023-02-06 13:41:23 +0100

    refactored home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Home from "./pages/Home";
 import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Fragment>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import NavBar from "../components/NavBar";
+
 import Banner from "../layouts/Banner";
 import HeroCarousel from "../layouts/HeroCarousel";
 import HeroMobile from "../layouts/HeroMobile";
@@ -12,7 +12,6 @@ import Footer from "../layouts/Footer";
 const Home = () => {
   return (
     <Fragment>
-      <NavBar />
       <Banner />
       <div className="md:overflow-x-scroll">
         <HeroCarousel />
